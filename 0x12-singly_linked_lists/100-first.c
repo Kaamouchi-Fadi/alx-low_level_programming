38bdee7515d006c94a5a5d5ba0796b6e8b3cc685
Kaamouchi-Fadi <Kaamouchi.fadi@gmail.com>
2023-04-27 14:57:15 -0700

    C - Singly linked lists

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * myStartupFun - Apply the constructor attribute to myStartupFun() so that it
+ *   is executed before main()
+ */
+void myStartupFun(void) __attribute__ ((constructor));
+
+/**
+ * myStartupFun - implementation of myStartupFun
+ */
+void myStartupFun(void){
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
