66d1c8cb1545096f6528f301f672260ae0784924
Kaamouchi-Fadi <Kaamouchi.fadi@gmail.com>
2023-04-27 15:34:06 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,8 +7,10 @@
  *
  * @head: A pointer to the first node of the list to free
  */
-void free_list(list_t *head){
-	if (head){
+void free_list(list_t *head)
+{
+	if (head)
+	{
 		free_list(head->next);
 		if (head->str)
 			free(head->str);
